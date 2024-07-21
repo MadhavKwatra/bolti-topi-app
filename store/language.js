6eb19c44c7cf606da48551a1c7b7480552c95d10
Madhav Kwatra <87752844+MadhavKwatra@users.noreply.github.com>
2024-07-21 20:06:20 +0530

    Added language context

---
+++ store/language.js
@@ -0,0 +1,16 @@
+import React, { createContext, useState } from "react";
+
+export const LanguageContext = createContext({
+  language: "english",
+  setLanguage: () => {},
+});
+
+export const LanguageProvider = ({ children }) => {
+  const [language, setLanguage] = useState("english");
+
+  return (
+    <LanguageContext.Provider value={{ language, setLanguage }}>
+      {children}
+    </LanguageContext.Provider>
+  );
+};
