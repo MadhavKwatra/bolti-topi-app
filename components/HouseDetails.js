0e7ab231c0484241c41341219a42a612ed36076b
Madhav Kwatra <87752844+MadhavKwatra@users.noreply.github.com>
2024-07-21 20:17:04 +0530

    changed styling of HouseDetails component

--- components/HouseDetails.js
+++ components/HouseDetails.js
@@ -1,4 +1,4 @@
-import { View, Text, Image } from "react-native";
+import { View, Text, Image, StyleSheet } from "react-native";
 import React from "react";
 
 const HouseDetails = ({ houseData }) => {
@@ -6,11 +6,29 @@ const HouseDetails = ({ houseData }) => {
 
   return (
     <View style={{ alignItems: "center", marginVertical: 10 }}>
-      <Image source={image} style={{ width: 100, height: 100, margin: 10 }} />
-      <Text>{title}</Text>
-      <Text>{description}</Text>
+      <Image
+        source={image}
+        style={{ width: 150, height: 150, margin: 10, resizeMode: "contain" }}
+      />
+      <Text
+        style={[
+          { fontStyle: "italic", fontWeight: "700", fontSize: 20 },
+          styles.text,
+        ]}
+      >
+        {title}
+      </Text>
+      <Text style={[styles.text, { fontWeight: "500", fontSize: 15 }]}>
+        {description}
+      </Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  text: {
+    color: "#D9BF8F",
+  },
+});
 
 export default HouseDetails;
